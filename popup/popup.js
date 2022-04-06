6714f8590706e16f6c7399a94d7299842c4d6d3a
vince-vibin <99386370+vince-vibin@users.noreply.github.com>
2022-04-06 21:26:15 +0200

    added handeling for Dropdown

--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("buttonMain").addEventListener("click", getCat)
+    //const dropDown = document.getElementById("animalsDrop")
+    document.getElementById("buttonMain").addEventListener("click", hanlder)
 })
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.msg === "backround_api_call") {
-            let responseURL = request.data.url
-            document.getElementById("catIMG").src = responseURL
-        }
+function hanlder() {
+    //const menu = document.getElementById("animalsDrop")
+    let val = document.getElementById("animalsDrop").value
+    if (val == "cat") {
+        console.log(val)
+        getCat()
+    } else if (val == "doggo") {
+        console.log(val)
+        getDoggo()
+    } else {
+        console.log(val)
     }
-);
-getCat()
+}
+
 
 function getCat() {
     let httpReq = new XMLHttpRequest()
     httpReq.open("GET", "https://api.thecatapi.com/v1/images/search", false)
     httpReq.send()
-
+    
     let response = JSON.parse(httpReq.responseText)[0]
     let responseURL = response["url"]
 
-    document.getElementById("catIMG").src = responseURL
+    document.getElementById("supportIMG").src = responseURL
 }
+
+// TODO get url write
+function getDoggo() {
+    let httpReq = new XMLHttpRequest()
+    httpReq.open("GET", "https://random.dog/woof.json", false)
+    httpReq.send()
+    
+    let response = JSON.parse(httpReq.responseText)[0]
+    let responseURL = response["url"]
+
+    document.getElementById("supportIMG").src = responseURL
+}
+
